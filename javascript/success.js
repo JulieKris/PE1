2ea9ade7b068ac5c44c8988f676faaffbf27a0cf
JulieKris <julkri02243@stud.noroff.no>
2025-11-06 13:56:02 +0100

    responsive

--- javascript/success.js
+++ javascript/success.js
@@ -15,6 +15,17 @@ document
 localStorage.setItem("cart", "[]");
 
 let cartItems = JSON.parse(localStorage.getItem("cart"));
+let customer = sessionStorage.getItem("customer");
+
+const successMessage = document.createElement("p");
+successMessage.innerHTML =
+  "Thank you for shopping with us, " +
+  customer +
+  "." +
+  "<br/>" +
+  " An email detailing your order will be sent promptly.";
+
+document.querySelector("#checkout-success").appendChild(successMessage);
 
 profile();
 cartFunctions();
